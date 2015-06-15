5a4c12c8548a8010cadaacd498fac93ea8c13194
Andrewkfb <andrewkfb@gmail.com>
2015-06-15 13:55:50 -0400

    Users(s)
    I think I just created a user database, model and controller all by
    myself!  Jk, definitely had some help…thanks internet :)

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  resources :users
   get 'welcome/index'
 
   get 'welcome/index'
+  get 'signup'  => 'users#new' 
+resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
